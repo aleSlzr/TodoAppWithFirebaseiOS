8a644fc6a4b03bf17074c7deb8940a548848f45b
Alejandro Salazar <salazar.cruz.alejandro@gmail.com>
2025-10-22 15:30:49 -0600

    - set up project

--- TodoAppWithFirebaseiOS/TodoAppWithFirebaseiOSApp.swift
+++ TodoAppWithFirebaseiOS/TodoAppWithFirebaseiOSApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TodoAppWithFirebaseiOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppStartingView()
         }
     }
 }
