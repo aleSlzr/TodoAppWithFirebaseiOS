aee2631e85ffb7ad594ac9f1569c4a7eda3a90ba
Alejandro Salazar <salazar.cruz.alejandro@gmail.com>
2025-10-22 11:21:17 -0600

    Initial Commit

---
+++ TodoAppWithFirebaseiOS/TodoAppWithFirebaseiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppWithFirebaseiOSApp.swift
+//  TodoAppWithFirebaseiOS
+//
+//  Created by Alejandro Salazar on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppWithFirebaseiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
